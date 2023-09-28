280f1ebc768039e6917767a96477e15515074247
yupingikub <82868182+yupingikub@users.noreply.github.com>
2023-09-28 23:51:19 +0200

    Update some student models

--- model_zoo.py
+++ model_zoo.py
@@ -2,23 +2,61 @@ import torchvision.models as models
 import torch.nn as nn
 import torch
 
-
-
-def get_mobilenet_small(pretrained=False, n_classes=4, greyscale=True):
+def get_moblienet2(pretrained = True, n_classes=4, greyscale=True):
 
     if pretrained:
-        model = models.mobilenet_v3_small(num_classes=n_classes, pretrained=pretrained)
-        model.classifier[-1] = nn.Linear(model.last_channel, n_classes)
+        model = models.mobilenet_v2(weights='MobileNet_V2_Weights.IMAGENET1K_V1')
+        for param in model.features.parameters():
+            param.requires_grad = False
+        model.features[0][0].in_channels = 1
+        model.classifier[1].out_features = n_classes
     else:
-        model = models.mobilenet_v3_small(num_classes=n_classes)
+        model = models.mobilenet_v2(weights=None)
+        model.features[0][0].in_channels = 1
+        model.classifier[1].out_features = n_classes
+        for param in model.features.parameters():
+            param.requires_grad = True
+    return model
+
+def get_moblienet3(pretrained = True, n_classes=4, greyscale=True):
+
+    if pretrained:
+        model = models.mobilenet_v3_small(weights='IMAGENET1K_V1')
+        for param in model.features.parameters():
+            param.requires_grad = False
+        model.features[0][0].in_channels = 1
+        model.classifier[3].out_features = n_classes
+    else:
+        model = models.mobilenet_v3_small(weights=None)
+        model.features[0][0].in_channels = 1
+        model.classifier[3].out_features = n_classes
+        for param in model.features.parameters():
+            param.requires_grad = True
+    return model
+def get_resnet18(pretrained = True, n_classes=4, greyscale=True):
+    if pretrained:
+        model = models.resnet18(weights = 'ResNet18_Weights.IMAGENET1K_V1')
+        for param in model.parameters():
+            param.requires_grad = False
+        model.fc.out_features = n_classes
+        model.conv1.in_channels = 1
+    else:
+        model = models.resnet18(weights = None)
+        model.fc.out_features = n_classes
+        model.conv1.in_channels = 1
+        for param in model.parameters():
+            param.requires_grad = True
     return model
 
 
 
-
 model_dict = {
-    "mobilenet_small": get_mobilenet_small
+    "mobilenet_V2": get_moblienet2,
+    "mobilenet_V2": get_moblienet3,
+    "resnet18": get_resnet18
 }
 
 if __name__ == "__main__":
-    model = get_mobilenet_small()
+    model_1 = get_moblienet2(4)
+    model_2 = get_resnet18(4)
+    model_3 = get_moblienet3(4)
