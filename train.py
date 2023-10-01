8ff70fb8a070bc13ca9435fa7d8ce380868b6d57
MoSpr <mospr10@yahoo.de>
2023-10-01 17:07:24 +0200

    fixed bugs; changed naming results

--- train.py
+++ train.py
@@ -36,17 +36,17 @@ def prep_directories():
 
 def run_experiment(config):
     try:
-        task_id = os.getenv("SLURM_ARRAY_TASK_ID")
+        task_id = int(os.getenv("SLURM_ARRAY_TASK_ID")) - 1
     except:
         print("not running sbatch array")
-        config_dict = make_grid(config)[0]
+        task_id = 0
     try:
         config_dict = make_grid(config)[task_id]
     except:
         print("not enough tasks available")
         return
     prep_directories()
-    train_dataloader, val_dataloader = get_dataloader(config_dict.get("data_dir", "real_data"), config_dict.get("bs", 64))
+    train_dataloader, val_dataloader = get_dataloader(config_dict.get("data_dir", "real_data"), config_dict.get("bs", 128), config_dict.get("train_split", 0.85))
     teacher_model = initialize_teacher_model(n_classes=config_dict.get("n_classes", 4))
     student_model = model_dict[config_dict.get("student_model_name")](config_dict.get("pretrained", False))
     optimizer = Adam(params=student_model.parameters(), lr=config_dict.get("lr", 0.001), weight_decay=config_dict.get("weight_decay", 0))
@@ -75,7 +75,7 @@ def initialize_teacher_model(model_checkpoint="models/VGG16_FF_2023_05_11_221428
 
     return model
 
-def get_dataloader(data_dir, batch_size=32):
+def get_dataloader(data_dir, batch_size=128, train_split=0.85):
     # Transformations
     input_size = 112
     transform = transforms.Compose([
@@ -86,8 +86,10 @@ def get_dataloader(data_dir, batch_size=32):
     # Dataset
     data = datasets.ImageFolder(root=data_dir, transform=transform)
     generator1 = torch.Generator().manual_seed(42)
-    test = len(data) - 1000 - 100
-    train_data, val_data, _ = random_split(data, [1000, 100, test], generator=generator1)
+    full_length = len(data)
+    train_len = int(full_length * train_split)
+
+    train_data, val_data = random_split(data, [train_len, full_length - train_len], generator=generator1)
     # Dataloader
     train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     val_dataloader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
@@ -107,6 +109,7 @@ def training_loop(teacher_model, student_model, optimizer, loss_fn, train_loader
     early_stopper = EarlyStopper(patience=5, min_delta=0.01)
     for epoch in range(1, num_epochs+1):
         train_t1 = time.time()
+        print(f"Start epoch {epoch}: {time.ctime(train_t1)}")
         student_model, train_loss, train_acc = train_epoch(teacher_model, student_model,
                                                    optimizer,
                                                    loss_fn,
@@ -117,6 +120,7 @@ def training_loop(teacher_model, student_model, optimizer, loss_fn, train_loader
                                                    loss_ratio,
                                                    greyscale)
         train_t2 = time.time()
+        print(f"End epoch {epoch}: {time.ctime(train_t2)}")
         train_times.append(train_t2 - train_t1)
         val_t1 = time.time()
         val_loss, val_acc = validate(teacher_model, student_model, loss_fn, val_loader, device, T, loss_ratio, greyscale)
@@ -142,15 +146,16 @@ def training_loop(teacher_model, student_model, optimizer, loss_fn, train_loader
     print(f"Best achieved val acc: {best_val_acc}")
     return best_model_params, train_losses, train_accs, val_losses, val_accs, train_times, val_times
 
-def temperature_softmax(logits, T):
-    logits = logits / T
-    return F.softmax(logits, dim=0)
-
 def train_epoch(teacher_model, student_model, optimizer, loss_fn, train_loader, val_loader, device, T, loss_ratio, greyscale):
     student_model.train()
     teacher_model.eval()
     train_loss_batches, train_acc_batches = [], []
+    times_preds = []
+    times_back = []
+    times_optimizer = []
+    times_track = []
     for batch_index, (x, y) in enumerate(train_loader, 1):
+        t1 = time.time()
         if not greyscale:
             student_x = torch.repeat_interleave(x, 3, 1)
             teacher_inputs, student_inputs, labels = x.to(device), student_x.to(device), y.to(device)
@@ -160,17 +165,27 @@ def train_epoch(teacher_model, student_model, optimizer, loss_fn, train_loader, 
             inputs, labels = x.to(device), y.to(device)
             student_pred = student_model.forward(inputs)
             teacher_pred = teacher_model.forward(inputs)
-
+        t2 = time.time()
+        times_preds.append(t2-t1)
         loss, hard_student_pred = compute_distillation_loss(teacher_pred, student_pred, labels, T, loss_ratio, loss_fn)
         loss.backward()
+        t3 = time.time()
+        times_back.append(t3-t2)
         optimizer.step()
+        t4 = time.time()
+        times_optimizer.append(t4-t3)
         optimizer.zero_grad()
         train_loss_batches.append(loss.item())
 
         hard_preds = hard_student_pred.argmax(dim=1)
         acc_batch_avg = (hard_preds.to(device) == labels).float().mean().item()
         train_acc_batches.append(acc_batch_avg)
-
+        t5 = time.time()
+        times_track.append(t5-t4)
+    print(f"pred: {sorted(times_preds)[-10:]}")
+    print(f"back: {sorted(times_back)[-10:]}")
+    print(f"opti: {sorted(times_optimizer)[-10:]}")
+    print(f"track: {sorted(times_track)[-10:]}")
     return student_model, sum(train_loss_batches)/len(train_loss_batches), sum(train_acc_batches)/len(train_acc_batches)
 
 def validate(teacher_model, student_model, loss_fn, val_loader, device, T, loss_ratio, greyscale):
@@ -197,12 +212,16 @@ def validate(teacher_model, student_model, loss_fn, val_loader, device, T, loss_
     return val_loss_cum/len(val_loader), val_acc_cum/len(val_loader)
 
 def compute_distillation_loss(teacher_pred, student_pred, labels, T, loss_ratio, loss_fn):
-    soft_student_pred = temperature_softmax(student_pred, T)
-    soft_teacher_pred = temperature_softmax(teacher_pred, T)
-    hard_student_pred = temperature_softmax(student_pred, 1)
-    distillation_loss = loss_fn(soft_student_pred, soft_teacher_pred)
-    student_loss = loss_fn(hard_student_pred, labels)
-    loss = loss_ratio * distillation_loss + (1- loss_ratio) * student_loss
+    soft_student_pred = student_pred / T
+    soft_teacher_pred = teacher_pred / T
+    # print(soft_student_pred.size())
+    # print(soft_teacher_pred.size())
+    soft_student_prob = F.log_softmax(soft_student_pred, dim=1)
+    soft_teacher_prob = F.log_softmax(soft_teacher_pred, dim=1)
+    distillation_loss = F.kl_div(soft_student_prob, soft_teacher_prob, reduction="batchmean", log_target=True) * T * T
+    student_loss = loss_fn(student_pred, labels)
+    loss = loss_ratio * distillation_loss + (1 - loss_ratio) * student_loss
+    hard_student_pred  = F.softmax(student_pred, dim=1)
     return loss, hard_student_pred
 
 def get_model_size(model):
@@ -230,7 +249,7 @@ def get_res_report(config_dict, train_losses, train_accs, val_losses, val_accs, 
         "val_acc": val_accs,
         "F1_score": [],
     }
-    output_file_name = construct_result_filename(config_dict)
+    output_file_name = construct_result_filename(config_dict, res_report)
     config_dict["filename"] = output_file_name
     for key, value in res_report.items():
         config_dict[key] = value
@@ -239,12 +258,13 @@ def get_res_report(config_dict, train_losses, train_accs, val_losses, val_accs, 
         json.dump(config_dict, fp, indent=2)
     return res_report
 
-def construct_result_filename(config_dict):
-    filename = [config_dict["student_model_name"], max(config_dict["val_acc"])]
+def construct_result_filename(config_dict, res_report):
+    filename = [config_dict["student_model_name"], f"best-{round(max(res_report['val_acc']), 6)}"]
+    ignore_list = ["student_model_name", "data_dir", "train_split", "pretrained", "greyscale", "n_classes", "num_epochs"]
     for key, value in config_dict.items():
-        if key != "student_model_name":
-            filename.append(f"{key}-{value}")
-    return "_".join(filename)
+        if key not in ignore_list:
+            filename.append(f"{key};{value}")
+    return "-".join(filename)
 
 
 
